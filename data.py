117cc7ae6312d96004fca0f9bdaced882b656996
LudvigJohansson99 <107184627+LudvigJohansson99@users.noreply.github.com>
2024-12-16 00:05:44 +0100

    Add files via upload

---
+++ data.py
@@ -0,0 +1,27 @@
+import requests
+import pandas as pd
+
+def fetch_data(api_key):
+    url = "https://www.alphavantage.co/query"
+    params = {
+        "function": "WTI",
+        "interval": "daily",
+        "datatype": "json",
+        "apikey": api_key
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    print(response.json())
+    # Convert to DataFrame and process
+    df = pd.DataFrame(data['data']).set_index('date')  # Adjust based on API response
+    
+    df = df.sort_index()  # Sort by date
+    df = df.ffill()  # Fill missing values forward
+    return df
+#H2KSZCLZNY6TP1MZ Is alphavantage API-key
+#H2KSZCLZNY6TP1MZ
+API_key = "H2KSZCLZNY6TP1MZ"
+results = fetch_data(API_key)
+results = results.ffill()
+#print(results[:5])
+results.to_json("oil_prices.json")  # Save as JSON file
